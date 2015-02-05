c6db23272421f975f32c6522a8bb5c869ab99253
Connor Clark <cjamcl@gmail.com>
2015-02-05 13:11:27 -0600

    moved swfobject license to distribution folder

--- build.rb
+++ build.rb
@@ -19,6 +19,5 @@ run_command "mkdir #{build_dir}"
 run_command "xcopy src #{build_dir}/src //E //I"
 run_command "xcopy dist #{build_dir}/ //E //I"
 run_command "copy README.md #{build_dir}"
-run_command "copy \"swfobject - LICENSE.md\" #{build_dir}"
 
 run_command "ruby #{build_dir}/test.rb"
